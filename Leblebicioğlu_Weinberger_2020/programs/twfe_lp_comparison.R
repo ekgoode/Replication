9de31ab979c9aca25555039199e62898c0671957
ekgoode <ethan.goode000@gmail.com>
2023-12-14 14:44:16 -0500

    Implemented TWFE from paper, and plotted coefs

--- Leblebicioğlu_Weinberger_2020/programs/twfe_lp_comparison.R
+++ Leblebicioğlu_Weinberger_2020/programs/twfe_lp_comparison.R
@@ -10,13 +10,15 @@
 # install.packages(c('data.table','tidyverse','viridis','fixest','haven'))
 # Data wrangling functions
 library(data.table)
+library(dplyr)
 library(haven)
 library(lubridate)
 library(stringr)
+library(forcats)
 library(readxl)
 
 # Visualization functions
-library(ggplot)
+library(ggplot2)
 library(viridis)
 
 # Estimation library
@@ -28,9 +30,10 @@ data_dir <- paste0(cdir,"/data")
 result_dir <- paste0(cdir,"/results")
 
 # Load data ----------------------------------------------------------------------------------------
-file_path <- paste0(data_dir,"/state_industry_replication.dfa")
-df <- read_dfa(file_path)
+file_path <- paste0(data_dir,"/state_industry_replication.dta")
+df <- read_dta(file_path)
 df <- as.data.table(df)
+blaw_df <- as.data.table(read_dta(paste0(data_dir, "/banking_law_indicators.dta")))
 
 # Process data -------------------------------------------------------------------------------------
 df[,indgroup := fcase(
@@ -60,37 +63,28 @@ df[, manufshare_fixed := mean(manufshare_fixed_t, na.rm = TRUE), by = state_name
 # Dropping temporary variables
 df[, c("manufshare_fixed_t", "empstateindgroup", "empstate", "indgroupshare_state") := NULL]
 
-# Collapse data and keep the first value of state_name
-df <- df[, lapply(.SD, function(x) if (is.numeric(x)) mean(x, na.rm = TRUE) else x[1L]), 
-         by = .(state_abbrev, year), .SDcols = setdiff(names(df), c("state_name"))]
-df[, state_name := first(state_name), by = .(state_abbrev, year)]
+# Collapse the data
+df <- df %>%
+  group_by(state_abbrev, year) %>%
+  summarise(
+    across(
+        c(laborsh_private,unemp,popgrowth,corptax,unionmem,HPI), \(x) mean(x, na.rm = TRUE)
+        ),
+        state_name = first(state_name)
+        )
+df <- as.data.table(df)
 
 # Dropping specific states and filtering years
 df <- df[!state_abbrev %in% c("AK", "HI", "SD", "DE", "DC"),]
 df <- df[year > 1969 & year < 1997,]
 
 # Creating new variables
-df[, loan2gsp := allloans_fdic / gsp / 1000]
-df[, loan2gsp_perc := loan2gsp * 100]
-df[, lnavgloanyield := log(avgloanyield)]
-df[, lnherf_deposits := log(1 + herf_deposits)]
-df[, lnherf_assets := log(1 + herf_assets)]
-df[, lnassetreturns := log(assetreturns)]
 df[, lnunemp := log(unemp)]
 df[, lnhpi := log(HPI)]
-df[, kint := kpwall]
-df[, lnkint := log(kint)]
-df[, stateid := .GRP, by = state_abbrev]
-df[, avgloanyield_perc := avgloanyield * 100]
-
-# Modifying variables
-df[, gsp_private := gsp_private / 1000]
-df[, comp_private := comp_private / 1000]
 
 # Merging with another data table (assuming banking_law_indicators is a data.table)
-df <- merge(df, banking_law_indicators, by = c("state_abbrev", "year"), all.x = TRUE)
-#df <- df[!is.na(_merge) & _merge != 2]
-#df[, _merge := NULL]
+blaw_df[,state_name := NULL]
+df <- merge(df, blaw_df, by = c("state_abbrev", "year"), all.x = TRUE)
 
 # Creating additional variables
 df[, zero := 0]
@@ -99,6 +93,155 @@ df[, zero2 := 0]
 
 
 # Run analysis Leblebicioğlu & Weinberger (2020) ---------------------------------------------------
+formula <- as.formula(
+    "laborsh_private ~ intbanking_yb89 + intbanking_yb67 + intbanking_yb45 + intbanking_yb23 + 
+  zero + intbanking_y0 + intbanking_ya1 + intbanking_ya23 + intbanking_ya45 + 
+  intbanking_ya67 + intbanking_ya89 + intbranching_yb89 + intbranching_yb67 + 
+  intbranching_yb45 + intbranching_yb23 + zero2 + intbranching_y0 + intbranching_ya1 + 
+  intbranching_ya23 + intbranching_ya45 + intbranching_ya67 + intbranching_ya89 + lnunemp + 
+  popgrowth + corptax + unionmem + lnhpi | year + statenum"
+)
+
+est <- feols(formula, df, vcov = ~statenum)
+
+# Panel A - Inter-state banking
+panelA_df <- as.data.frame(summary(est)$coeftable)
+panelA_df$variable <- rownames(panelA_df)
+rownames(panelA_df) <- NULL
+panelA_df <- panelA_df %>% filter(str_detect(variable,"intbanking_"))
+panelA_df <- rbind(panelA_df, c(0,0,0,0,"intbanking_yb1"))
+panelA_df$Estimate <- as.numeric(panelA_df$Estimate)
+panelA_df$`Std. Error` <- as.numeric(panelA_df$`Std. Error`)
+
+panelA_df <- panelA_df %>%
+mutate(
+    upper95 = Estimate + (1.96 * `Std. Error`),
+    lower95 = Estimate - (1.96 * `Std. Error`)
+)
+
+panelA_df <- panelA_df %>%
+mutate(
+    period = case_when(
+        variable == "intbanking_yb89" ~ "-8, -9",
+        variable == "intbanking_yb67" ~ "-6, -7",
+        variable == "intbanking_yb45" ~ "-4, -5",
+        variable == "intbanking_yb23" ~ "-2, -3",
+        variable == "intbanking_yb1" ~ "-1",
+        variable == "intbanking_y0" ~ "0",
+        variable == "intbanking_ya1" ~ "1",
+        variable == "intbanking_ya89" ~ "8, 9",
+        variable == "intbanking_ya67" ~ "6, 7",
+        variable == "intbanking_ya45" ~ "4, 5",
+        variable == "intbanking_ya23" ~ "2, 3",
+        default = NULL
+    )
+)
+panelA_df$period <- factor(
+    panelA_df$period,
+    levels = c(
+        "-8, -9",
+        "-6, -7",
+        "-4, -5",
+        "-2, -3",
+        "-1",
+        "0",
+        "1",
+        "2, 3",
+        "4, 5",
+        "6, 7",
+        "8, 9"
+    ))
+
+panelA_plt <- ggplot(data = panelA_df, aes(x=period,y=Estimate)) +
+geom_pointrange(aes(ymin = lower95, ymax = upper95),color="#21918c", position = position_dodge(width=0.5)) +
+geom_hline(aes(yintercept=0),linetype="dashed",linewidth=1, color="#440154") +
+labs(x="Years Before and After Reform",y="Coefficients",title="Inter-state Banking, Leblebicioğlu & Weinberger (2020)") + 
+theme(
+    panel.grid.major = element_line(color = "grey90", linetype = "solid"),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(linetype = "solid", fill = NA, color = "black"),
+    axis.title = element_text(size = 18, face = "bold", color = "black"),
+    axis.text = element_text(size = 16, color = "black"),
+    plot.title = element_text(size = 18, face="bold",color="black")
+)
+
+ggsave(
+    paste0(result_dir,"/fig2_panelA.png"),
+    panelA_plt,
+    units = "in",
+    height = 8, width = 12, dpi = 300,
+    device = "png"
+)
+
+# Panel B - Intra-state Branching
+panelB_df <- as.data.frame(summary(est)$coeftable)
+panelB_df$variable <- rownames(panelB_df)
+rownames(panelB_df) <- NULL
+panelB_df <- panelB_df %>% filter(str_detect(variable,"intbranching_"))
+panelB_df <- rbind(panelB_df, c(0,0,0,0,"intbranching_yb1"))
+panelB_df$Estimate <- as.numeric(panelB_df$Estimate)
+panelB_df$`Std. Error` <- as.numeric(panelB_df$`Std. Error`)
+
+panelB_df <- panelB_df %>%
+mutate(
+    upper95 = Estimate + (1.96 * `Std. Error`),
+    lower95 = Estimate - (1.96 * `Std. Error`)
+)
+
+panelB_df <- panelB_df %>%
+mutate(
+    period = case_when(
+        variable == "intbranching_yb89" ~ "-8, -9",
+        variable == "intbranching_yb67" ~ "-6, -7",
+        variable == "intbranching_yb45" ~ "-4, -5",
+        variable == "intbranching_yb23" ~ "-2, -3",
+        variable == "intbranching_yb1" ~ "-1",
+        variable == "intbranching_y0" ~ "0",
+        variable == "intbranching_ya1" ~ "1",
+        variable == "intbranching_ya89" ~ "8, 9",
+        variable == "intbranching_ya67" ~ "6, 7",
+        variable == "intbranching_ya45" ~ "4, 5",
+        variable == "intbranching_ya23" ~ "2, 3",
+        default = NULL
+    )
+)
+panelB_df$period <- factor(
+    panelB_df$period,
+    levels = c(
+        "-8, -9",
+        "-6, -7",
+        "-4, -5",
+        "-2, -3",
+        "-1",
+        "0",
+        "1",
+        "2, 3",
+        "4, 5",
+        "6, 7",
+        "8, 9"
+    ))
+
+panelB_plt <- ggplot(data = panelB_df, aes(x=period,y=Estimate)) +
+geom_pointrange(aes(ymin = lower95, ymax = upper95),color="#21918c", position = position_dodge(width=0.5)) +
+geom_hline(aes(yintercept=0),linetype="dashed",linewidth=1, color="#440154") +
+labs(x="Years Before and After Reform",y="Coefficients",title="Intra-state Branching, Leblebicioğlu & Weinberger (2020)") + 
+theme(
+    panel.grid.major = element_line(color = "grey90", linetype = "solid"),
+    panel.grid.minor = element_blank(),
+    panel.border = element_rect(linetype = "solid", fill = NA, color = "black"),
+    axis.title = element_text(size = 18, face = "bold", color = "black"),
+    axis.text = element_text(size = 16, color = "black"),
+    plot.title = element_text(size = 18, face="bold",color="black")
+)
+
+ggsave(
+    paste0(result_dir,"/fig2_panelB.png"),
+    panelB_plt,
+    units = "in",
+    height = 8, width = 12, dpi = 300,
+    device = "png"
+)
+
 # Run analysis Dube, Girardi, Jorda, & Taylor (2023) -----------------------------------------------
 
 
